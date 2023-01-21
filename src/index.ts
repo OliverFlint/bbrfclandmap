a88f0d55b3509f087c68c534e7f71bc66fcdc5a1
Oliver Flint <oliverflint@gmail.com>
2023-01-21 21:42:38 +0000

    command line with coord args

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,20 @@ import yargs from "yargs/yargs";
     ne: { type: "string", demandOption: true, alias: "northeast" },
   });
   const argv = await args.argv;
-  console.log(argv.sw);
+  let swCorOrds: { lng: number; lat: number } = { lng: 0, lat: 0 };
+  let neCorOrds: { lng: number; lat: number } = { lng: 0, lat: 0 };
+  const swGroups = argv.sw.matchAll(/^([0-9\.]*)\,([0-9\.]*)$/gm).next();
+  swCorOrds = { lng: swGroups.value[1], lat: swGroups.value[2] };
+  console.log(`South West ${JSON.stringify(swCorOrds)}`);
+  const neGroups = argv.ne.matchAll(/^([0-9\.]*)\,([0-9\.]*)$/gm).next();
+  neCorOrds = { lng: neGroups.value[1], lat: neGroups.value[2] };
+  console.log(`North East ${JSON.stringify(neCorOrds)}`);
+
+  // todo: calculate se & nw coords
+
+  // todo: calculate rows & columns
+
+  // todo: calculate rows/columns min & max
+
+  // todo: calculate rectangles within coords and min/max
 })();
