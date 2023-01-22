38a0d98c088f6d099dd44869f57818292908e919
Oliver Flint <oliverflint@gmail.com>
2023-01-22 20:34:11 +0000

    se/nw coords

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,20 @@ import yargs from "yargs/yargs";
     ne: { type: "string", demandOption: true, alias: "northeast" },
   });
   const argv = await args.argv;
-  let swCorOrds: { lng: number; lat: number } = { lng: 0, lat: 0 };
-  let neCorOrds: { lng: number; lat: number } = { lng: 0, lat: 0 };
+  let swCoOrds: { lat: number; lng: number } = { lng: 0, lat: 0 };
+  let neCoOrds: { lat: number; lng: number } = { lng: 0, lat: 0 };
   const swGroups = argv.sw.matchAll(/^([0-9\.]*)\,([0-9\.]*)$/gm).next();
-  swCorOrds = { lng: swGroups.value[1], lat: swGroups.value[2] };
-  console.log(`South West ${JSON.stringify(swCorOrds)}`);
+  swCoOrds = { lat: swGroups.value[1], lng: swGroups.value[2] };
+  console.log(`South West ${JSON.stringify(swCoOrds)}`);
   const neGroups = argv.ne.matchAll(/^([0-9\.]*)\,([0-9\.]*)$/gm).next();
-  neCorOrds = { lng: neGroups.value[1], lat: neGroups.value[2] };
-  console.log(`North East ${JSON.stringify(neCorOrds)}`);
+  neCoOrds = { lat: neGroups.value[1], lng: neGroups.value[2] };
+  console.log(`North East ${JSON.stringify(neCoOrds)}`);
 
   // todo: calculate se & nw coords
+  const seCoOrds = { lat: swCoOrds.lat, lng: neCoOrds.lng };
+  console.log(`South East ${JSON.stringify(seCoOrds)}`);
+  const nwCoOrds = { lat: neCoOrds.lat, lng: swCoOrds.lng };
+  console.log(`North West ${JSON.stringify(nwCoOrds)}`);
 
   // todo: calculate rows & columns
 
